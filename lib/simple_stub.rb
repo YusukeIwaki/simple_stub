adb049b4940ce3b8537439be7da811bdede3330e
YusukeIwaki <iwaki@i3-systems.com>
2021-09-19 22:11:50 +0900

    Initial commit

--- lib/simple_stub.rb
+++ lib/simple_stub.rb
@@ -1,6 +1,5 @@
 require "simple_stub/version"
 
 module SimpleStub
-  class Error < StandardError; end
-  # Your code goes here...
+
 end
---
+++ test/simple_stub/test_version.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+
+class TestVersion < Minitest::Test
+  def test_version_present
+    assert defined?(SimpleStub::VERSION)
+    assert Gem::Version.new(SimpleStub::VERSION) >= Gem::Version.new('0.0.1')
+  end
+end
